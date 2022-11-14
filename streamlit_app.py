cc5e674bfff51032a92be5059fe75621d7834810
Mellymel <117777744+NITSNOWFLAKE@users.noreply.github.com>
2022-11-14 14:52:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvoce Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered',frui_choice)
 
 import requests
